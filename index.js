ddcc30eb4e33d5036fab84e776d29e192ba81392
anirudh yagnik <ani789yagnik@gmail.com>
2019-10-07 11:54:30 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express=require('require')
+const path=require('path')
+
+const app=express()
+
+app.use('/',express.static(path.join(__dirname,'/public')))
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,"public/index.html"))
+})
+
+app.get('/blog',(req,res)=>{
+    res.sendFile(path.join(__dirname,"public/blog.html"))
+})
+
+app.listen(3000,()=>{console.log('listening at 3000')})
+
